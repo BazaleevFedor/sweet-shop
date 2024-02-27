f63ac823c0160f6bbb2c144c48c02355c1715080
BazaleevFedor <bazaleevf@gmail.com>
2024-02-27 17:22:03 +0300

    add menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,17 +9,25 @@ export const Navbar = () => {
 
     return (
         <div className={ styles.navbarContainer }>
-            <div className={ styles.navbar }>
+            <div className={ classnames(styles.navbar) }>
                 { configuration.navbarTitles.map((title, index) => (
-                    <Link key={index} className={ `${styles.navbar} ${pathname === title.href ? styles.active : ''}` } href={ title.href }>{ title.text }</Link>
+                    <Link key={index} className={ `${pathname === title.href ? styles.active : ''}` } href={ title.href }>{ title.text }</Link>
                 )) }
             </div>
 
-            <svg className={ classnames(styles.menu, 'svgThemeLine') } width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2 2H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
-                <path d="M2 12H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
-                <path d="M2 22H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
-            </svg>
+            <div className={ classnames(styles.navbarMobil) } id='navbarMobil'>
+                { configuration.navbarTitles.map((title, index) => (
+                    <Link key={index} className={ `${pathname === title.href ? styles.active : ''}` } href={ title.href }>{ title.text }</Link>
+                )) }
+            </div>
+
+            <button className={ styles.menu } onClick={ () => document.getElementById('navbarMobil')?.classList.toggle(styles.active) }>
+                <svg className={ classnames('svgThemeLine') } width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2 2H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
+                    <path d="M2 12H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
+                    <path d="M2 22H27" stroke="#131313" strokeWidth="4" strokeLinecap="round"/>
+                </svg>
+            </button>
         </div>
     );
 }
